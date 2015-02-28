e8e34df8e7620803783b4fc20074b1cb84815322
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2015-02-28 14:44:02 +0000

    Disable vsync.

--- assets/main_menu/scroll_title.gd
+++ assets/main_menu/scroll_title.gd
@@ -1,13 +1,16 @@
 extends Label
 
+var pos
+
 func _ready():
     set_process(true)
+    pos = get_pos()
 
-func _process(delta):
-    var pos = get_pos()
-    
+func _process(delta):    
     if pos.x >= 200:
         set_process(false)
         return
+        
+    pos.x +=delta * 200
 
-    set_pos(Vector2(pos.x + delta * 200, pos.y))
+    set_pos(pos)
